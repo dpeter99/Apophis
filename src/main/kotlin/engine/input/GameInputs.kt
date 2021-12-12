735c834ac6ec3123b3c2260fc85ee6ea6accc21a
dpeter99 <dpeter99@gmail.com>
2021-12-12 15:42:14 +0100

    Update

--- src/main/kotlin/engine/input/GameInputs.kt
+++ src/main/kotlin/engine/input/GameInputs.kt
@@ -1,6 +1,5 @@
 package engine.input
 
-import com.sun.org.apache.xpath.internal.operations.Bool
 import glm_.vec1.Vec1
 import glm_.vec2.Vec2
 
@@ -25,7 +24,7 @@ class GameInputs : InputDefinition() {
     var inGameMap = inputMap {
         action<Vec2>("Move"){
             compositeBind {
-
+                XPos =
             }
         }
 
@@ -33,8 +32,11 @@ class GameInputs : InputDefinition() {
             bind(Devices.Mouse.Pos)
         }
 
-        action<Float>("Speed"){
-
+        action<Vec1>("Speed"){
+            compositeBind {
+                XPos = bind(Devices.Keyboard.Q)
+                XNeg = bind(Devices.Keyboard.E)
+            }
         }
 
     }
@@ -51,56 +53,75 @@ class InputMap{
 }
 
 
-
-class Action<T>{
+@InputTagMarker
+open class Action<T>{
 
     enum class ActionType{
         Value,
         Change
     }
 
+    fun <B : Binding<T>> internal_bind(ctor:()-> B, init: B.() -> Unit = {}):B{
+        var b = ctor();
+        b.init();
+        return b;
+    }
+
     fun bind(b:DeviceInput<T>, init: Binding<T>.() -> Unit = {}): Binding<T>{
-        var b = DeviceBinding(b)
-        b.init()
-        return b
+        return internal_bind({DeviceBinding<T>(b)}, init);
     }
 
 }
 
-fun Action<Float>.compositeBind(init: CompositeBindingVec1.() -> Unit = {}): CompositeBindingVec1{
-    var b = DeviceBinding(b)
-    b.init()
-    return b
+fun Action<Vec1>.compositeBind(init: CompositeBindingVec1.() -> Unit = {}): CompositeBindingVec1 {
+    return this.internal_bind({CompositeBindingVec1()},init);
 }
 
-
-fun <T> Action<T>.compositeBind(init: CompositeBindingVec2.() -> Unit) where T : glm_.vec2.Vec2{
-
+fun Action<Vec2>.compositeBind(init: CompositeBindingVec2.() -> Unit = {}): CompositeBindingVec2 {
+    return this.internal_bind({CompositeBindingVec2()},init);
 }
 
+@InputTagMarker
 abstract class Binding<T>{
     abstract fun processEvent(e:Unit): T
 }
 
 class DeviceBinding<T>(val dev: DeviceInput<T>) : Binding<T>(){
-
     override fun processEvent(e: Unit): T {
         TODO("Not yet implemented")
+    }
+}
+
+abstract class CompositeBinding_t<T> : Binding<T>(){
+
+    fun bind(init: Binding<Vec1>.() -> Unit = {}): Binding<Vec1>{
+
     }
 
 }
 
 class CompositeBindingVec2 : Binding<Vec2>() {
-    lateinit var XPos: Binding<Boolean | Vec1>
-    lateinit var XNeg: Binding
-    lateinit var YPos: Binding
-    lateinit var YNeg: Binding
+    lateinit var XPos: Binding<Vec1>
+    lateinit var XNeg: Binding<Vec1>
+    lateinit var YPos: Binding<Vec1>
+    lateinit var YNeg: Binding<Vec1>
+
+    override fun processEvent(e: Unit): Vec2 {
+        TODO("Not yet implemented")
+    }
 }
 
 class CompositeBindingVec1 : Binding<Vec1>() {
-    lateinit var XPos: Binding
-    lateinit var XNeg: Binding
+    lateinit var XPos: Binding<Vec1>
+    lateinit var XNeg: Binding<Vec1>
+
+    override fun processEvent(e: Unit): Vec1 {
+        TODO("Not yet implemented")
+    }
 }
+
+
+
 
 class DeviceInput<T> (val path: String){
 
@@ -112,10 +133,16 @@ object Devices{
     object Keyboard{
         private const val path = Devices.path + "Keyboard"
 
-        val W = DeviceInput<Boolean>("${this.path}/W")
-        const val S = "${this.path}/S"
-        const val A = "${this.path}/A"
-        const val D = "${this.path}/D"
+        val Q = DeviceInput<Vec1>("${this.path}/Q")
+        val W = DeviceInput<Vec1>("${this.path}/W")
+        val E = DeviceInput<Vec1>("${this.path}/E")
+        val R = DeviceInput<Vec1>("${this.path}/R")
+        val T = DeviceInput<Vec1>("${this.path}/T")
+        val Z = DeviceInput<Vec1>("${this.path}/Z")
+        val A = DeviceInput<Vec1>("${this.path}/A")
+        val S = DeviceInput<Vec1>("${this.path}/S")
+        val D = DeviceInput<Vec1>("${this.path}/D")
+        val F = DeviceInput<Vec1>("${this.path}/F")
     }
 
     object Mouse{
